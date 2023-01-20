ca9409b21d783b3983b9b39e21a5c9aeeda76376
Ivan Aires <ivanaires32@gmail.com>
2023-01-20 11:55:28 -0300

    Projeto atualizado

--- js/script.js
+++ js/script.js
@@ -69,10 +69,9 @@ function mostrarBatePapo() {
         `;
 
             BatePapo.innerHTML = BatePapo.innerHTML + template;
-        } else {
+        } else if (conversas[i].type === "private_message" && conversas[i].to !== nome.name) {
             let template = `
-        <div class="mensagem none" data-test="message">
-            <p><strong>${conversas[i].from}</strong> para <strong>${conversas[i].to}</strong>: ${conversas[i].text}</p>
+        <div class="none" data-test="message">
         </div>
         `;
 
